feaa2a722244e3c914cfa4c047cc047961242370
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-24 15:00:43 +0000

    hot fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(
-    function () {
+  promise
+    .then(function () {
       console.log('Got a response from the API');
-      return {status: 200, body: 'success',};
-    },
-    function () {
+      return { status: 200, body: "success" };
+    })
+    .catch(function () {
       return new Error({});
-    }
-  );
+    });
 }
