82864495ed324d4f52ced032927eca68e8763890
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-24 23:20:13 +0000

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { creatUser, uploadPhoto } from "./utils.js";
+
+export default function handleProfileSignup() {
+  let body, fname, lname;
+  Promise.all([creatUser(), uploadPhoto()])
+    .then((response) => {
+      console.log(
+        `${response[1].body} ${response[0].firstName} ${response[0].lastName}`
+      );
+    })
+    .catch((err) => console.log(err));
+}
