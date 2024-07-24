3b79c28db6aea8caca8b2233e8af5be841dd7379
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-24 14:57:10 +0000

    fixes

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(
     function () {
-      console.log("Got a response from the API");
-      return { status: 200, body: 'success' };
+      console.log('Got a response from the API');
+      return {status: 200, body: 'success',};
     },
     function () {
       return new Error({});
