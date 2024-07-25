ae8054259bbec82e892fb8d766e4c32c7544b0ac
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-25 00:43:02 +0000

    res

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,9 @@ import { creatUser, uploadPhoto } from "./utils.js";
 export default function handleProfileSignup() {
   Promise.all([creatUser(), uploadPhoto()])
     .then((response) => {
+      const [user, photo] = response;
       console.log(
-        `${response[1].body} ${response[0].firstName} ${response[0].lastName}`
+        `${photo.body} ${user.firstName} ${user.lastName}`
       );
     })
     .catch(() => console.log('Signup system offline'));
