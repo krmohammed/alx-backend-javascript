d04000fe5049c01cd3f7700b97c450978c668380
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-25 00:21:01 +0000

    resolving promises

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise(function (resolve, reject) {
-    resolve({ firstName, lastName });
-  });
+  return Promise.resolve({ firstName, lastName });
 }
