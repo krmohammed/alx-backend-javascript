838931e07059f22969e692cc4133792a5638c836
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-25 00:58:16 +0000

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((response) =>
+    response.map((resp) => ({
+      status: resp.status,
+      value: resp.status === 'fulfilled' ? resp.value : resp.reason,
+    }))
+  );
+}
