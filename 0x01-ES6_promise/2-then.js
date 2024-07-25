6c5b83ea0cc4f2d9538432cff33e89d847a2f35b
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-25 01:05:34 +0000

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   promise
     .then(function () {
       console.log('Got a response from the API');
-      return { status: 200, body: 'success',};
+      return { status: 200, body: 'success' };
     })
     .catch(function () {
       return new Error({});
