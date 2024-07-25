d8a6016b5599d292fd40d2f519ce7cf303563349
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-25 03:02:03 +0000

    task 8.

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw new Error('cannot divide by zero');
-  } else {
-    return numerator / denominator;
   }
+  return numerator / denominator;
 }
