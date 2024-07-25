570ebd4dc97ff0f568bae0d3c212a9545b15cf78
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-25 03:09:28 +0000

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     const res = mathFunction();
     queue.push(res);
   } catch (error) {
-    queue.push(error)
+    queue.push(error.toString())
   } finally {
     queue.push('Guardrail was processed');
   }
