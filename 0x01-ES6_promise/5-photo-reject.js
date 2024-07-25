bdba3e177840df00f6ea2981395d94aa12199366
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-25 00:17:18 +0000

    rejecting promises

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,3 @@
 export default function uploadPhoto(filename) {
-  return new Promise(function(resolve, reject) {
-    reject(`${filename} cannot be processed`)
-  });
+  return Promise.reject(`${filename} cannot be processed`);
 }
