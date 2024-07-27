ddc8c1aa4d50b106a74ec5c39edff3c9dcfc565c
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-27 06:16:16 +0000

    lint fixes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -16,9 +16,11 @@ export default class HolbertonCourse {
       throw new Error('Students must be an object');
     }
   }
+
   get name() {
     return this._name;
   }
+
   set name(name) {
     if (typeof name === 'string') {
       this._name = name;
@@ -26,9 +28,11 @@ export default class HolbertonCourse {
       throw new Error('Name must be a string');
     }
   }
+
   get length() {
     return this._length;
   }
+
   set length(length) {
     if (typeof length === 'number') {
       this._length = length;
@@ -36,9 +40,11 @@ export default class HolbertonCourse {
       throw new Error('Length must be a number');
     }
   }
+
   get students() {
     return this._students;
   }
+
   set students(students) {
     if (typeof students === 'object') {
       this._students = students;
