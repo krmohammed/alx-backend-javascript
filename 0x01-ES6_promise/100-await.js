50e50267da2d609001f5c87b48cc57e558a161b3
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-07-29 09:28:38 +0000

    task 100 - promise

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+async function asyncUploadUser() {
+  const user = await createUser();
+  const photo = await uploadPhoto();
+
+  return {
+    photo,
+    user,
+  };
+}
+
+export default asyncUploadUser;
