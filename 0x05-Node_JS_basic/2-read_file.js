4af231f32a5af9394d16a759090aef23c733f5dd
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-09-18 07:09:15 +0000

    task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('node:fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\r\n');
+    const valid = lines.filter(line => line.trim() !== "")
+    const header = valid.shift();
+
+    if (valid.length <= 0) {
+      console.log('Number of students: 0');
+      return;
+    }
+
+    const studentsField = {}
+    valid.forEach(line => {
+      const fields = line.split(',');
+      const fname = fields[0];
+      const field = fields[3];
+
+      if (!studentsField[field]) {
+        studentsField[field] = [];
+      }
+      studentsField[field].push(fname);
+    });
+
+    console.log(`Number of students: ${valid.length}`);
+    for (const [field, students] of Object.entries(studentsField)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
