912272e4024f3f883193fa6458577cd8d624f156
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-09-18 09:50:53 +0000

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,13 @@
 const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
 const port = 1245;
 
-
 const app = createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  res.write('Hello Holberton School!');
+  res.end();
 });
 
-app.listen(port, hostname, () => {
-});
+app.listen(port);
 
 module.exports = app;
