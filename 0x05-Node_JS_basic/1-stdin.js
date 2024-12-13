9fc6255e5374a7a6ea1289a890bf3f7953c51c3c
krmohammed <mohrahman558@gmail.com>
2024-12-13 19:52:31 +0000

    task 1 fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,9 @@
-const readline = require('readline');
-const process = require('process');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data}`);
 });
 
-rl.on('close', () => {
+process.stdin.on('close', () => {
   console.log('This important software is now closing');
 });
