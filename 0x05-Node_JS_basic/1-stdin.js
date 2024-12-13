47790fbee4a6617716011dd99da9386cc12934f9
krmohammed <mohrahman558@gmail.com>
2024-12-13 17:02:30 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,6 @@
 console.log('Welcome to ALX, what is your name?');
 
-
 process.stdin.on('data', (data) => {
-    console.log(`Your name is: ${data}`);
-    process.exit();
-})
+  console.log(`Your name is: ${data}`);
+  process.exit();
+});
