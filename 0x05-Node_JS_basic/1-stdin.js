ac6d779321a6417214cc96c841e91eb02ee8e610
krmohammed <mohrahman558@gmail.com>
2024-12-13 17:32:15 +0000

    fixing task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,17 @@
-const readline = require('readline')
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-})
+});
 
 console.log('Welcome to ALX, what is your name?');
 
 rl.on('line', (input) => {
   console.log(`Your name is: ${input}`);
   rl.close();
-})
+});
 
 rl.on('close', () => {
   console.log('This important software is now closing');
-})
+});
