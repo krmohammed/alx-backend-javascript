88c63ceb0e4c14022378f8ff0294c118a0570284
krmohammed <mohrahman558@gmail.com>
2024-12-14 17:42:03 +0000

    task 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,25 @@
+const { createServer } = require('http');
+const countStudents = require('./3-read_file_async');
+
+const port = 1245;
+const db = 'database.csv';
+
+const app = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students');
+    countStudents(process.argv[2])
+    .then((data) => {
+      console.log(data);
+    });
+    res.end();
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
