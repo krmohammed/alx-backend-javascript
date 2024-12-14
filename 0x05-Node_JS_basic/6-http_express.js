70279f6ce0c1eebbe0c24979f5aee1ab74c2ab81
krmohammed <mohrahman558@gmail.com>
2024-12-14 22:45:11 +0000

    hot fix

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-app.get('/', (res) => {
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
