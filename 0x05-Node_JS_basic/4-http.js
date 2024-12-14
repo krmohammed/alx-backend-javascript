58be70e0bee6167665ee1b334712debe26d48013
krmohammed <mohrahman558@gmail.com>
2024-12-14 17:17:14 +0000

    task fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,12 @@
+const { response } = require('express');
 const { createServer } = require('http');
 
 const port = 1245;
 
 const app = createServer((req, res) => {
-  if (req.url) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
-  }
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  console.log(res.statusCode);
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port);
