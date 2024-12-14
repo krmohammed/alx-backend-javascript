3e0f559bb8a80bde05b966614923bd6c45b916c8
krmohammed <mohrahman558@gmail.com>
2024-12-14 14:17:10 +0000

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,42 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const content = await fs.readFile(path, 'utf-8');
+    const data = content.trim().split('\r\n');
+    data.shift();
+
+    const inner = [];
+    data.forEach((val) => {
+      const a = val.trim();
+      const b = a.split(',');
+      inner.push(b);
+    });
+    const fields = [];
+    inner.forEach((val) => {
+      if (!fields.includes(val[val.length - 1])) {
+        fields.push(val[val.length - 1]);
+      }
+    });
+    const studentsByFields = {};
+    fields.forEach((val) => {
+      const b = inner.filter((v) => v.includes(val));
+      const c = [];
+      b.forEach((v) => {
+        c.push(v[0]);
+      });
+      studentsByFields[val] = c;
+    });
+    const students = inner.length;
+    console.log(`Number of students: ${students}`);
+    for (const a in studentsByFields) {
+      if (Object.hasOwn(studentsByFields, a)) {
+        console.log(`Number of students in ${a}: ${studentsByFields[a].length}. List: ${studentsByFields[a].join(', ')}`);
+      }
+    }
+  } catch {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
